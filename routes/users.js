8d304a43244c512fe4076d7cc91885eccc8b2971
chinonso williams <williamschinonso11@gmail.com>
2018-06-28 04:58:21 +0100

    added my addPost route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var userController =require ('../controllers/UserControllers');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/create', userController.addPost);
+
 
 module.exports = router;
